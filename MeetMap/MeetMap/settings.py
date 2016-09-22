a9082497b28154ea2df177803ca50883d0192b7f
James <jjov8020@uni.sydney.edu.au>
2016-09-22 20:53:13 +1000

    Implemented PostgreSQL as database and added test models from django tutorial

--- MeetMap/MeetMap/settings.py
+++ MeetMap/MeetMap/settings.py
@@ -77,9 +77,13 @@ WSGI_APPLICATION = 'MeetMap.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'webapp_db',
+            'USER': 'postgres',
+            'PASSWORD': 'Whatcunt1',
+            'HOST': 'localhost',
+            'PORT': '8000',
+        }
 }
 
 
