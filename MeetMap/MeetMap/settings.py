e14eb4541358691b999a35c69153a0eaaa8187a0
Karthik <KarBalasubramanian@gmail.com>
2016-09-23 13:21:26 +1000

    Resetting master branch to SQLite
    Resetting master branch to SQLite and will create new branch for PostgreSQL. Once database is ready merge back into master.

--- MeetMap/MeetMap/settings.py
+++ MeetMap/MeetMap/settings.py
@@ -76,14 +76,18 @@ WSGI_APPLICATION = 'MeetMap.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #        'NAME': 'webapp_db',
+    #        'USER': 'postgres',
+    #        'PASSWORD': 'Whatcunt1',
+    #        'HOST': 'localhost',
+    #        'PORT': '8000',
+    #    }
     'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'webapp_db',
-            'USER': 'postgres',
-            'PASSWORD': 'elec3609db',
-            'HOST': 'localhost',
-            'PORT': '8000',
-        }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
