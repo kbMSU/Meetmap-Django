695731683f9b88f806041d0af08ab26a532b19ce
Karthik <KarBalasubramanian@gmail.com>
2016-09-23 22:56:41 +1000

    Fixed admin
    Fixed issue with admin page not showing

--- MeetMap/mainapp/urls.py
+++ MeetMap/mainapp/urls.py
@@ -5,6 +5,6 @@ from . import views
 app_name = 'mainapp'
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),
     url(r'^test/', views.test, name='test'),
+    url(r'^', views.index, name='index'),
 ]
