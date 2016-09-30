b5a7c55dae34bb9dfe78a3225e20fe37908e4c57
Karthik <KarBalasubramanian@gmail.com>
2016-09-30 15:48:06 +1000

    Added models
    Added the models from the SDS. Ideally we should not need any changes to this.

--- MeetMap/mainapp/admin.py
+++ MeetMap/mainapp/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Profile
+from .models import UserProfile, Interest, Location, Event
 
-admin.site.register(Profile)
+admin.site.register(UserProfile)
+admin.site.register(Interest)
+admin.site.register(Location)
+admin.site.register(Event)
